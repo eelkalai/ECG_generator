d2c764dd3631aadb5b72c2726db92bc03d156e99
Eliran <eliranelkalai@gmail.com>
2023-05-22 13:10:27 +0300

    ECG simple generator

--- ECG_generator.py
+++ ECG_generator.py
@@ -2,14 +2,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import neurokit2 as nk  # Load the package
 from ecg_plot import ecg_plot
+from PIL import Image, ImageDraw
 
-def GetECG():
+width, height = 1600, 720
+
+
+def GetECG(sample_rate):
     heart_rate = np.random.randint(50, 120)
     print(heart_rate)
-    ecgs = nk.ecg_simulate(duration=8, sampling_rate=500,noise=0.1, heart_rate=heart_rate,method="multileads")
-    return np.transpose(ecgs.to_numpy())
-    plt.savefig('./hh.png')
-wGetECG()
-GetECG()
-GetECG()
-GetECG()
+    ecg = nk.ecg_simulate(duration=8, sampling_rate=sample_rate, noise=0.1, heart_rate=heart_rate, method="multileads")
+    return np.transpose(ecg.to_numpy())
+
+
+def createLabel(x1, x2, y1, y2):
+    center_x = (x1 + x2) / 2 / width
+    center_y = (y1 + y2) / 2 / height
+    width_rec = (x2 - x1) / width
+    height_rec = (y2 - y1) / height
+    label = f"0 {center_x:.6f} {center_y:.6f} {width_rec:.6f} {height_rec:.6f}"
+    return label
+
+
+def SaveTrainVal(index, train_or_val):
+    scale_size = 40
+    duration = 8
+    sample_rate = 100
+    row_height = 6
+    columns = 2
+    ecgs = GetECG(sample_rate=sample_rate)
+    labels = ""
+    y_max = 0
+    y_min = 0
+    for i, ecg in enumerate(ecgs):
+        y_max = ecg.max()
+        y_min = ecg.min()
+        labels += createLabel(int(i/6)*800, (int(i/6)+1)* 800, scale_size * (1.5 + 3*(i % 6)-y_max),scale_size * (1.5 + 3*(i % 6)-y_min)) + "\n"
+    ecg_plot.plot(ecgs, sample_rate=sample_rate, row_height=row_height, columns=columns, title='ECG 12')
+    filename = 'train_data/images/'+train_or_val+f'/ECG_gen_{index + 1}.png'
+    plt.savefig(filename)
+    plt.close()
+    filename = 'train_data/labels/'+train_or_val+f'/ECG_gen_{index + 1}.txt'
+    with open(filename, 'w') as f:
+        f.write(labels)
+
+
+def drawOnData(x, y):
+    img = Image.open("./train/square_4.png")
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((800, x, 1600, y), fill=None, outline='black')
+    img.show()
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
 import random
+
+from ecg_plot import ecg_plot
+
+import ECG_generator as ecgen
 
 
 def createLabel(x1, x2, y1, y2, pagesize):
@@ -12,39 +17,52 @@ def createLabel(x1, x2, y1, y2, pagesize):
 
 
 # Set the size of the square images
-width, height = 512, 512
+width, height = 1600, 720
 
 # Set the number of images to generate
 num_images = 100
 
-# Loop over the number of images and generate each one
-for i in range(num_images):
-    # Create a new image with a white background
-    img = Image.new('RGB', (width, height), color='white')
-    existing_rectangles = []
-    labels = ""
 
-    # Generate a random size and position for the square
-    for j in range(10):
-        print(j)
-        x_size = random.randint(50, 200)
-        y_size = random.randint(50, 200)
-        x = random.randint(0, width - x_size)
-        y = random.randint(0, height - y_size)
-        overlap = False
-        for rectangle in existing_rectangles:
-            if rectangle[0] < x + x_size and rectangle[1] > x and rectangle[2] < y + y_size and rectangle[3] > y:
-                overlap = True
-        if overlap:
-            continue
-        labels += createLabel(x, x + x_size, y, y + y_size, width) + "\n"
-        existing_rectangles.append([x, x + x_size, y, y + y_size])
-        draw = ImageDraw.Draw(img)
-        draw.rectangle((x, y, x + x_size, y + y_size), fill=None, outline='black')
+def Rectangle():
+    for i in range(num_images):
+        # Create a new image with a white background
+        img = Image.new('RGB', (width, height), color='white')
+        existing_rectangles = []
+        labels = ""
+        ecg =[]
+        ecg_plot.plot(ecg, sample_rate=500, title='ECG 12')
 
-    # Save the image
-    filename = f'train/square_{i + 1}.png'
-    img.save(filename)
-    filename = f'val/square_{i + 1}.txt'
-    with open(filename, 'w') as f:
-        f.write(labels)
+        # Generate a random size and position for the square
+        for j in range(10):
+            print(j)
+            x_size = random.randint(50, 200)
+            y_size = random.randint(50, 200)
+            x = random.randint(0, width - x_size)
+            y = random.randint(0, height - y_size)
+            overlap = False
+            for rectangle in existing_rectangles:
+                if rectangle[0] < x + x_size and rectangle[1] > x and rectangle[2] < y + y_size and rectangle[3] > y:
+                    overlap = True
+            if overlap:
+                continue
+            labels += createLabel(x, x + x_size, y, y + y_size, width) + "\n"
+            existing_rectangles.append([x, x + x_size, y, y + y_size])
+            draw = ImageDraw.Draw(img)
+            draw.rectangle((x, y, x + x_size, y + y_size), fill=None, outline='black')
+
+        # Save the image
+        filename = f'train/square_{i + 1}.png'
+        img.save(filename)
+        filename = f'val/square_{i + 1}.txt'
+        with open(filename, 'w') as f:
+            f.write(labels)
+
+
+def main():
+    for i in range(100):
+        ecgen.SaveTrainVal(i,'train')
+    for i in range(10):
+        ecgen.SaveTrainVal(i, 'val')
+if __name__ == "__main__":
+    main()
+
